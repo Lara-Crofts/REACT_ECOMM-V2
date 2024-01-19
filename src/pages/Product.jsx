40b88ada4d78b158cc0615931fad10c488044631
Jessica Contreras Ayala <hoshostars@hotmail.com>
2024-01-19 01:32:51 -0500

    filter functionality and css products

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,33 +1,8 @@
-// Product.jsx
-
-// import React from 'react';
-// import ProductList from '../components/ProductList'; // Adjust the path based on your project structure
-
-// const ProductPage = () => {
-//   return (
-//     <div>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//         <br></br>
-//       <h1>Product Page</h1>
-//       <ProductList />
-//       <br></br>
-
-//       <h1>Product Page</h1>
-//     </div>
-//   );
-// };
-
-// export default ProductPage;
-
-
 // ProductPage.jsx
 
 import React, { useState } from 'react';
 import ProductList from '../components/ProductList';
-import PriceFilter from '../components/PriceFilter';
+// import PriceFilter from '../components/PriceFilter';
 
 const ProductPage = () => {
   const [filterOption, setFilterOption] = useState('all');
@@ -40,13 +15,17 @@ const ProductPage = () => {
     <div>
       <br></br>
       <br></br>
+      <br></br>
+      <br></br>
       <h1>Product Page</h1>
 
       {/* Render the PriceFilter component and pass the handleFilterChange function */}
-      <PriceFilter onFilterChange={handleFilterChange} />
+      {/* <PriceFilter onFilterChange={handleFilterChange} /> */}
 
       {/* Pass the filterOption to the ProductList component */}
-      <ProductList filterOption={filterOption} />
+      {/* <ProductList filterOption={filterOption} /> */}
+
+      <ProductList />
 
       <br></br>
     </div>
